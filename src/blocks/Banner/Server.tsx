eaed71fe97833b1223ea678d1b305b07e2d1a8a6
ismifariezz <119034031+ismifariezz@users.noreply.github.com>
2024-09-09 11:11:27 +0800

    Added SearchBox

--- src/blocks/Banner/Server.tsx
+++ src/blocks/Banner/Server.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Input } from '@/components/ui/input';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState, useEffect } from 'react';
 
 export default function BannerServer({ title, bannerImage, carousel }) {
@@ -30,6 +33,20 @@ export default function BannerServer({ title, bannerImage, carousel }) {
                     <div className="flex flex-col gap-4 px-4 md:px-0">
                         <h1 className="text-white text-3xl md:text-5xl font-bold">{title}</h1>
                     </div>
+                    <div className="relative flex items-center gap-4">
+                <div className="absolute left-0 top-0 h-full flex items-center pl-4">
+                  <FontAwesomeIcon
+                    icon={faMagnifyingGlass}
+                    className="text-zinc-400"
+                    size="lg"
+                  />
+                </div>
+                <Input
+                  type="text"
+                  placeholder="Masukkan kata kunci carian ..."
+                  className="text-zinc-400 p-6 pl-12 text-lg md:text-xl rounded-lg"
+                />
+              </div>
 
                     {/* Carousel images */}
                     <div className="w-full h-[200px] md:w-[640px] md:h-[276px] overflow-hidden relative">
